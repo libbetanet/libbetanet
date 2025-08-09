47875d23c77e7ef2adb39faff558bf813d293845
lilaf <lilafian74@proton.me>
2025-08-09 16:17:44 -0500

    create frame, uint24_t, and empty placeholder varint

---
+++ src/frame.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "int.h"
+
+#define CIPHERTEXT_SIZE 65535
+
+typedef struct {
+    uint24_t length; // should always be CIPHERTEXT_SIZE ?
+    uint8_t type;
+    varint stream_id;
+    uint8_t ciphertext[CIPHERTEXT_SIZE];
+} BN_FRAME;
---
+++ src/int.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct {
+    
+} varint;
+
+typedef struct {
+    uint32_t v : 24;
+} uint24_t;
